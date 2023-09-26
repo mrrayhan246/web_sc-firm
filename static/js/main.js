a0459e919925de270753f51419ea598671405547
MR R A Y H A N <126187036+mrrayhan246@users.noreply.github.com>
2023-09-26 09:51:12 +0600

    RR

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,27 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+
+
+
+
+//============== Scroll Reveal =================//
+//============== Scroll Reveal =================//
+ScrollReveal({
+    reset: true,
+    distance: '40px',
+    duration: 2000,
+    delay: 400
+});
+
+ScrollReveal().reveal('.srt-1st', {delay: 200, origin: 'right'});
+
+ScrollReveal().reveal('.sr-1st', {delay: 300, origin: 'top'});
+ScrollReveal().reveal('.sr-2nd', {delay: 300, origin: 'left'});
+ScrollReveal().reveal('.sr-3rd', {delay: 300, origin: 'right'});
+ScrollReveal().reveal('.sr-4th', {delay: 300, origin: 'left'});
+
+
+
+
