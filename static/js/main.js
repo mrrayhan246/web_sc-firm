a572bd68002a7dada95b97b488a73733dee1b1cf
MR R A Y H A N <126187036+mrrayhan246@users.noreply.github.com>
2023-09-26 10:07:19 +0600

    RR

--- static/js/main.js
+++ static/js/main.js
@@ -13,16 +13,16 @@ window.addEventListener("scroll", function(){
 ScrollReveal({
     reset: true,
     distance: '40px',
-    duration: 2000,
+    duration: 1500,
     delay: 400
 });
 
 ScrollReveal().reveal('.srt-1st', {delay: 200, origin: 'right'});
 
-ScrollReveal().reveal('.sr-1st', {delay: 300, origin: 'top'});
-ScrollReveal().reveal('.sr-2nd', {delay: 300, origin: 'left'});
-ScrollReveal().reveal('.sr-3rd', {delay: 300, origin: 'right'});
-ScrollReveal().reveal('.sr-4th', {delay: 300, origin: 'left'});
+ScrollReveal().reveal('.sr-1st', {delay: 200, origin: 'top'});
+ScrollReveal().reveal('.sr-2nd', {delay: 200, origin: 'left'});
+ScrollReveal().reveal('.sr-3rd', {delay: 200, origin: 'right'});
+ScrollReveal().reveal('.sr-4th', {delay: 200, origin: 'left'});
 
 
 
